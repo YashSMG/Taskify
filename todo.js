759b83d86680efec396089fd91bfacd98d1e7225
Yash_Gadhwe <89290742+YashSMG@users.noreply.github.com>
2024-04-01 22:46:18 +0530

    Update todo.js
    issue resolved

--- todo.js
+++ todo.js
@@ -2,45 +2,36 @@ const taskEntry = document.getElementById('taskEntry');
 const taskList = document.getElementById('task-list');
 
 function addTask() {
-	if(taskEntry.value == ""){
-		alert("Enter a task");
-	}
-	else{
-		let li = document.createElement("li");
-		li.innerHTML = taskEntry.value;
-		taskList.appendChild(li);
-		let span = document.createElement('span');
-		span.innerHTML = "&#10060";
-		li.appendChild(span);
-		saveData();
-
-	}
-	taskEntry.value = "";
+    if (taskEntry.value === "") {
+        alert("Enter a task");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = taskEntry.value;
+        taskList.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "&#10060";
+        li.appendChild(span);
+        saveData();
+    }
+    taskEntry.value = "";
 }
 
-taskList.addEventListener {
-	"click",
-	function (event) {
-		if (event.target.tagName === "li") {
-			event.target.classList.toggle("checked");
-			saveData();
-		}
-		else if (event.target.tagName === "span") {
-			event.target.parentElement.remove();
-			saveData();
-		}
-		console.log(event);
-	},
-	false
-};
+taskList.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+        saveData();
+    } else if (event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+}, false);
 
 function saveData() {
-	localStorage.setItem("myTask", taskList.innerHTML);
-
+    localStorage.setItem("myTask", taskList.innerHTML);
 }
 
 function displayData() {
-	taskList.innerHTML = localStorage.getItem("myTask");
-
+    taskList.innerHTML = localStorage.getItem("myTask");
 }
+
 displayData();
